76387410b00610da544a3ec9d83b1a1aec5b8051
Adrien Guichard <adrien.guichard@taack.org>
2024-08-03 03:13:48 +0200

    fix build for Intellij 2024.2.RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 
 plugins {
     id("java")
-    id("org.jetbrains.intellij.platform") version "2.0.0-beta8"
+    id("org.jetbrains.intellij.platform") version "2.0.0"
 }
 group "org.taack"
-version "1.6.6-beta8"
+version "1.6.6"
 
 repositories {
     mavenCentral()
@@ -26,34 +26,7 @@ dependencies {
         zipSigner()
         instrumentationTools()
 
-//        plugin("org.intellij.groovy", "4.0.14")//version.get())
-
-//        plugins.set(listOf(/* Plugin Dependencies */"com.intellij.java", "org.intellij.groovy"))
-//        id = "org.taack.taack-autocomplete"
-        version = "1.6.6-beta8"
-//        name = "TaackUiASTAutocomplete"
+        version = "1.6.6"
         description = "Use with TaackUI Framework to recognize compiler extensions."
-//        changeNotes =
-//            """
-//            Compile for release 2024.2.beta8 ...
-//            """.trimIndent()
-
     }
 }
-
-//patchPluginXml {
-//    changeNotes.set(provider {
-//        """2024.X version.<br><em>Ensure Taack AST Transformations get completion</em>"""
-//    })
-//}
-
-//signPlugin {
-////    password = System.getenv("PRIVATE_KEY_PASSWORD")
-////    certificateChain = new File('/home/auo/intellij/chain.crt').text
-////    privateKey = new File('/home/auo/intellij/private.pem').text
-//}
-
-
-//publishPlugin {
-//    token = System.getenv("PUBLISH_TOKEN")
-//}
